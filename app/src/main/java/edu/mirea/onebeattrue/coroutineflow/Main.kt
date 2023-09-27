c57050b42f8890027d7ac87d28198baa729a8182
Vitaly <onebeattrue@gmail.com>
2023-09-28 01:37:58 +0300

    Еще 2 FlowBuilder'а: 1) flowOf (под капотом flow()) 2) flow {} с методом добавления элементов emit()

--- app/src/main/java/edu/mirea/onebeattrue/coroutineflow/Main.kt
+++ app/src/main/java/edu/mirea/onebeattrue/coroutineflow/Main.kt
@@ -1,15 +1,21 @@
 package edu.mirea.onebeattrue.coroutineflow
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 
 suspend fun main() {
-    val flow = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).asFlow() // аналог sequence, используется для работы с coroutine'ами
-    flow.filter { it.isPrime() }
-        .map { it * 10 }
-        .collect { println(it) }
+    getFlowByFlowOfBuilder().filter { it.isPrime() }
+        .collect { println(it) } // терминальный метод (инициализирует поток)
+
+    println()
+
+    getFlowByFlowBuilder().filter { it.isPrime() }.collect { println(it) }
 }
 
 suspend fun Int.isPrime(): Boolean {
@@ -19,4 +25,17 @@ suspend fun Int.isPrime(): Boolean {
         if (this % i == 0) return false
     }
     return true
+}
+
+fun getFlowByFlowOfBuilder(): Flow<Int> {
+    return flowOf(1, 2, 3)
+}
+
+fun getFlowByFlowBuilder(): Flow<Int> {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    return flow {
+        list.forEach {
+            emit(it)
+        }
+    }
 }
